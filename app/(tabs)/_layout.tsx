e5f343f4282ef649cb28abf450d841a7b43542e6
Sebastian Christen <70528963+SebastianChristen@users.noreply.github.com>
2024-11-11 13:15:05 +0000

    fix navbar layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,17 +28,17 @@ export default function TaskList() {
     <View style={styles.container}>
       <View style={styles.header}>
         {/* Gelb: Logo */}
-        <View style={styles.logoContainer}>
+        <View style={styles.imageContainer}>
           <Image
             source={require("../../assets/images/Ohout Logo.png")}
-            style={[styles.logo, { width: 150, height: 150 }]}
+            style={styles.image}
           />
         </View>
 
         {/* Blau: Suchleiste */}
         <View style={styles.searchContainer}>
           <TextInput
-            style={[styles.searchBar, { width: '80%', height: 80, fontSize: 30 }]}
+            style={styles.searchBar}
             value={searchQuery}
             onChangeText={handleSearchChange}
             onSubmitEditing={handleSearchSubmit}
@@ -46,21 +46,16 @@ export default function TaskList() {
           />
         </View>
 
-        {/* Grün: Login Button */}
-        <View 
-          style={[styles.rightContainer, { width: 80, height: 80}]}>
-          <TouchableOpacity style={styles.loginButton}>
-            <Button
-              title="Login"
-              onPress={() => console.log("Navigiere zur Login-Seite")}
-            />
-          </TouchableOpacity>
+          {/* Grün: Login Button */}
+        <TouchableOpacity style={styles.loginButton}>
+          <Button
+            title="Login" onPress={() => console.log('Navigate to login page')} />
+        </TouchableOpacity>
 
-          {/* Rot: Warenkorb Icon */}
-          <TouchableOpacity style={styles.cartButton}>
-            <Ionicons name="cart" size={24} color="black" />
-          </TouchableOpacity>
-        </View>
+         {/* Rot: Warenkorb Icon */}
+        <TouchableOpacity style={styles.iconContainer}>
+          <Ionicons name="cart" size={24} color="black" />
+        </TouchableOpacity>
       </View>
 
       {/* Tabs Component */}
@@ -101,7 +96,7 @@ export default function TaskList() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 170},
+    paddingTop: 120},
   header: {
     flexDirection: "row",
     alignItems: "center",
@@ -113,33 +108,29 @@ const styles = StyleSheet.create({
     zIndex: 10,
     backgroundColor: "white",
   },
-  logoContainer: {
-    paddingLeft: 10,
+  imageContainer: {
+    padding: 10,
   },
-  logo: {
-    width: 50,
-    height: 50,
-    resizeMode: "contain",
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 20,
   },
   searchContainer: {
     flex: 1,
-    alignItems: "center",
+    marginHorizontal: 10,
   },
   searchBar: {
-    width: "80%", // Zentriert und feste Breite
     borderWidth: 1,
     borderRadius: 8,
     padding: 10,
     borderColor: "#ccc",
   },
-  rightContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
   loginButton: {
     marginRight: 10,
+    width: 90,
   },
-  cartButton: {
-    paddingRight: 10,
+  iconContainer: {
+    padding: 20,
   },
 });
